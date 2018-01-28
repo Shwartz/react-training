191bbedc98c69bc4f28a98dc383ae10dd0918d8c
Andris Shvarcs <andris.svarcs@gmail.com>
2018-01-28 12:17:21 +0000

    Passing method to click component (stateless)

--- src/App.js
+++ src/App.js
@@ -13,6 +13,14 @@ class App extends Component {
 
   switchNameHandler = () => {
     console.log('Was clicked!');
+    //this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        {name: 'Maximilian', age: '28'},
+        {name: 'Manu', age: '29'},
+        {name: 'Stephanie', age: '25'},
+      ]
+    })
   };
 
   render() {
@@ -24,9 +32,17 @@ class App extends Component {
         <h1>Hi, I'm React App</h1>
         <p>This is really working</p>
         <button onClick={this.switchNameHandler}>Switch Name</button>
-        <Person name={persons[0].name} age={persons[0].age} />
-        <Person name={persons[1].name} age={persons[1].age}>My Hobbies: Racing</Person>
-        <Person name={persons[2].name} age={persons[2].age} />
+        <Person
+          name={persons[0].name}
+          age={persons[0].age} />
+        <Person
+          name={persons[1].name}
+          age={persons[1].age}
+          click={this.switchNameHandler}
+        >My Hobbies: Racing</Person>
+        <Person
+          name={persons[2].name}
+          age={persons[2].age} />
       </div>
     );
   }
