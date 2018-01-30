1211e047643bd9c9dd6644d5335208f900b7f466
shvarcs <andris.svarcs@avanade.com>
2018-01-30 16:23:16 +0000

    Section 7, How to use HOC

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,26 +1,27 @@
 import React from 'react';
 import styles from './Cockpit.css';
+import Aux from '../../hoc/Aux';
 
 
 const cockpit = (props) => {
   const assignedClasses = [];
-  let btnClass = '';
+  let btnClass = styles.Button;
   if (props.showPersons) {
-    btnClass = styles.Red;
+    btnClass = [styles.Button, styles.Red].join(' ');
   }
 
   if (props.persons.length <= 2) assignedClasses.push(styles.red);
   if (props.persons.length <= 1) assignedClasses.push(styles.bold);
 
   return (
-    <div className={styles.Cockpit}>
+    <Aux>
       <h1>{props.title}</h1>
       <p className={assignedClasses.join(' ')}>This is really working</p>
       <button
         className={btnClass}
         onClick={props.clicked}>Toggle Persons
       </button>
-    </div>
+    </Aux>
   )
 };
 
