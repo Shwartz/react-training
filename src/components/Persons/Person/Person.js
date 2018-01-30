888e3f358d7f257e3523a9b937ab90f526191892
shvarcs <andris.svarcs@avanade.com>
2018-01-30 14:07:21 +0000

    Section 7, Converting stateless components to stateful

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import styles from './Person.css';
 
-const person = (props) => {
-  const {name, age, changed} = props;
 
-  return (
-    <div className={styles.Person}>
-      <p onClick={props.click}>I'm a {name} and I am {age} years old.</p>
-      <p>{props.children}</p>
+class Person extends React.Component {
+
+  render() {
+    const {name, age, changed} = this.props;
+
+    return <div className={styles.Person}>
+      <p onClick={this.props.click}>I'm a {name} and I am {age} years old.</p>
+      <p>{this.props.children}</p>
       <input type="text" onChange={changed} value={name} />
     </div>
-  )
-};
+  }
+}
 
-export default person;
+export default Person;
--- src/containers/App.js
+++ src/containers/App.js
@@ -52,6 +52,7 @@ class App extends Component {
     return (
       <div className={styles.App}>
         <Cockpit
+          title={this.props.appTitle}
           persons={this.state.persons}
           clicked={this.togglePersonsHandler}
           showPersons={this.state.showPersons}/>
