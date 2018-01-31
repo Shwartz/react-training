520deb0abb17317d17d541590090ede6bbc82c39
shvarcs <andris.svarcs@avanade.com>
2018-01-31 11:14:33 +0000

    Section 8, Creating new git branch with clean start for new App

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,80 +1,17 @@
-import React, {PureComponent} from 'react';
-import Persons from '../components/Persons/Persons';
-import Cockpit from '../components/Cockpit/Cockpit';
-import withClass from '../hoc/withClass';
-import Aux from '../hoc/Aux';
+import React, {Component} from 'react';
 import styles from './App.css';
 
-class App extends PureComponent {
+class App extends Component {
   constructor(props) {
     super(props);
     console.log('props: ', props);
   }
-  state = {
-    persons: [
-      {id: 'kds03', name: 'Max', age: 28},
-      {id: 'hdhd3', name: 'Manu', age: 29},
-      {id: 'sdw3', name: 'Stephanie', age: 26},
-    ],
-    showPersons: false,
-    toggleClicked: 0,
-  };
-
-  /*shouldComponentUpdate (nextProps, nextState) {
-    return nextState.persons !== this.state.persons || nextState.showPersons !== this.state.showPersons;
-  }*/
-
-  deletePersonHandler = (personIndex) => {
-    //const persons = this.state.persons.slice();
-    const persons = [...this.state.persons];
-    persons.splice(personIndex, 1);
-    this.setState({persons: persons})
-  };
-
-  nameChangeHandler = (event, id) => {
-    const persons = [...this.state.persons];
-    const personIndex = this.state.persons.findIndex(p => p.id === id);
-    const person = {...this.state.persons[personIndex]};
-
-    person.name = event.target.value;
-    persons[personIndex] = person;
-
-    this.setState({persons: persons})
-  };
-
-  togglePersonsHandler = () => {
-    console.log('show persons');
-    const doesShow = this.state.showPersons;
-    this.setState((prevState, props) => {
-      return {
-        showPersons: !doesShow,
-        toggleClicked: prevState.toggleClicked + 1,
-      }
-    })
-  };
 
   render() {
-    let personsBlock = null;
-    if (this.state.showPersons) {
-      personsBlock = <Persons
-        persons={this.state.persons}
-        clicked={this.deletePersonHandler}
-        changed={this.nameChangeHandler}
-      />;
-    }
-
     return (
-      <Aux styles={styles.App}>
-        <button onClick={()=>{this.setState({showPersons: true})}}>Show Persons</button>
-        <Cockpit
-          title={this.props.appTitle}
-          persons={this.state.persons}
-          clicked={this.togglePersonsHandler}
-          showPersons={this.state.showPersons}/>
-        {personsBlock}
-      </Aux>
+      <div className={styles.App}>Empty app</div>
     );
   }
 }
 
-export default withClass(App, styles.App);
+export default App;
