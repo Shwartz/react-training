0debd7100ff12d082b231d1224b7460bde4d117a
shvarcs <andris.svarcs@avanade.com>
2018-02-02 13:54:24 +0000

    Section 8, if no ingredients add sign to add them

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -4,16 +4,24 @@ import styles from './Burger.css';
 import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-  const transformedIngredients = Object.keys(props.ingredients)
+
+  let transformedIngredients = Object.keys(props.ingredients)
     .map(igKey => {
       return [...Array(props.ingredients[igKey])].map((_, i) => {
-        return <BurgerIngredient key={igKey + i} type={igKey} />;
+        return <BurgerIngredient key={igKey + i} type={igKey}/>;
       });
-    });
-
-  console.log('props', props);
-  console.log('transformedIngredients', transformedIngredients);
-
+    })
+    // at this point we have [Array(0), Array(n), N)
+    // we need flatten array to get that info out from Array and bet to array
+    .reduce((arr, el) => {
+      console.log('1. arr, el', arr, el);
+      //-> [{key, props, f}, {n}]
+      return arr.concat(el);
+    }, []);
+  console.log('transformIngredients', transformedIngredients);
+  if (transformedIngredients.length === 0) {
+    transformedIngredients = <p>Ingredients</p>
+  }
 
   return (
     <div className={styles.Burger}>
