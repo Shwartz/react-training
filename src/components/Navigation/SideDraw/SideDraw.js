50c6dc3783a3d1b94e9b88d4e2e87c4d74f6a7cf
shvarcs <andris.svarcs@avanade.com>
2018-02-05 16:19:21 +0000

    Section 8, Working state of mob menu

--- src/components/Navigation/SideDraw/SideDraw.js
+++ src/components/Navigation/SideDraw/SideDraw.js
@@ -3,18 +3,27 @@ import React from 'react';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../../Navigation/NavigationItems/NavigationItems';
 import styles from './SideDraw.css';
+import Backdrop from '../../UI/Backdrop/Backdrop';
+import Aux from '../../../hoc/Aux';
 
 const sideDraw = (props) => {
+  let attachedClasses = [styles.SideDraw, styles.Close];
+  if(props.open) {
+    attachedClasses = [styles.SideDraw, styles.Open];
+  }
 
   return (
-    <div className={styles.SideDraw}>
-      <div className={styles.Logo}>
-        <Logo/>
+    <Aux>
+      <Backdrop show={props.open} clicked={props.closed}/>
+      <div className={attachedClasses.join(' ')}>
+        <div className={styles.Logo}>
+          <Logo/>
+        </div>
+        <nav>
+          <NavigationItems/>
+        </nav>
       </div>
-      <nav>
-        <NavigationItems/>
-      </nav>
-    </div>
+    </Aux>
   );
 };
 export default sideDraw;
