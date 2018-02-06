e40a41355657c0ce75584cc3589b3549380e241d
shvarcs <andris.svarcs@avanade.com>
2018-02-06 10:57:54 +0000

    Section 9: adding new files

--- src/App.js
+++ src/App.js
@@ -1,20 +1,12 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 
-import Layout from './hoc/Layout/Layout';
-import BurgerBuilder from './containers/BurgerBuilder/BurgerBuilder';
-import styles from './App.css';
+import Blog from './containers/Blog/Blog';
 
 class App extends Component {
-  /*constructor(props) {
-    super(props);
-  }*/
-
   render() {
     return (
-      <div className={styles.App}>
-        <Layout>
-          <BurgerBuilder>Burger builder</BurgerBuilder>
-        </Layout>
+      <div className="App">
+        <Blog />
       </div>
     );
   }
