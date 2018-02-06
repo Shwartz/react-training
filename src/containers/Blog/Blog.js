dd5764b498b3264550d6fedcfaf2d65d1a9b35e2
shvarcs <andris.svarcs@avanade.com>
2018-02-06 11:46:03 +0000

    Section 9: Using Axios for Ajax requests

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
+import axios from 'axios';
 
 import Post from '../../components/Post/Post';
 import FullPost from '../../components/FullPost/FullPost';
@@ -6,23 +7,30 @@ import NewPost from '../../components/NewPost/NewPost';
 import styles from './Blog.css';
 
 class Blog extends Component {
-    render () {
-        return (
-            <div>
-                <section className={styles.Posts}>
-                    <Post />
-                    <Post />
-                    <Post />
-                </section>
-                <section>
-                    <FullPost />
-                </section>
-                <section>
-                    <NewPost />
-                </section>
-            </div>
-        );
-    }
+  componentDidMount() {
+    axios.get('http://jsonplaceholder.typicode.com/posts')
+      .then(response => {
+        console.log('response: ', response);
+      })
+  }
+
+  render() {
+    return (
+      <div>
+        <section className={styles.Posts}>
+          <Post/>
+          <Post/>
+          <Post/>
+        </section>
+        <section>
+          <FullPost/>
+        </section>
+        <section>
+          <NewPost/>
+        </section>
+      </div>
+    );
+  }
 }
 
 export default Blog;
