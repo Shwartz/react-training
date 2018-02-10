e874433eebda2f1b7e1b95040872efede5e830c3
Andris Shvarcs <andris.svarcs@gmail.com>
2018-02-10 11:51:51 +0000

    Section 11 Routing: Adding first router component

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import {Route} from 'react-router-dom';
 
 import Posts from './Posts/Posts';
 import styles from './Blog.css';
@@ -16,7 +17,9 @@ class Blog extends Component {
             </ul>
           </nav>
         </header>
-        <Posts/>
+        {/*<Route path="/" exact render={() => <h1>Home</h1>} />
+        <Route path="/" render={() => <h1>Home 2</h1>} />*/}
+        <Route path="/" exact component={Posts} />
       </div>
     );
   }
