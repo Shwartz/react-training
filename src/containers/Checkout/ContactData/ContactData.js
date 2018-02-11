9ee8e03a356a17e03fb30c0b8845056608aa90a6
Andris Shvarcs <andris.svarcs@gmail.com>
2018-02-11 12:21:03 +0000

    Section 13 Forms: Creating Form config object

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -4,14 +4,61 @@ import Button from '../../../components/UI/Button/Button';
 import styles from './ContactDate.css';
 import axios from '../../../axios-order';
 import Spinner from '../../../components/UI/Spinner/Spinner';
+import Input from '../../../components/UI/Input/Input';
 
 class ContactData extends Component {
   state = {
-    name: '',
-    email: '',
-    address: {
-      street: '',
-      postalCode: ''
+    orderForm: {
+      name: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'You name'
+        },
+        value: ''
+      },
+      zipCode: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'ZIP Code'
+        },
+        value: ''
+      },
+      street: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Street'
+        },
+        value: ''
+      },
+      country: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Country'
+        },
+        value: ''
+      },
+      email: {
+        elementType: 'input',
+        elementConfig: {
+          type: 'text',
+          placeholder: 'Email'
+        },
+        value: ''
+      },
+      deliveryMethod: {
+        elementType: 'select',
+        elementConfig: {
+          options: [
+            {value: 'fastest', displayValue: 'Fastest'},
+            {value: 'cheapest', displayValue: 'Cheapest'}
+          ]
+        },
+        value: ''
+      },
     },
     loading: false,
   };
@@ -42,10 +89,10 @@ class ContactData extends Component {
   render() {
     let form = (
       <form>
-        <input type="text" name="name" placeholder="Your name"/>
-        <input type="email" name="email" placeholder="Your email"/>
-        <input type="text" name="street" placeholder="Your street"/>
-        <input type="text" name="postal" placeholder="Your postal code"/>
+        <Input inputtype="input" type="text" name="name" placeholder="Your name"/>
+        <Input inputtype="input" type="email" name="email" placeholder="Your email"/>
+        <Input inputtype="input" type="text" name="street" placeholder="Your street"/>
+        <Input inputtype="input" type="text" name="postal" placeholder="Your postal code"/>
         <Button btnType="Success" clicked={this.orderHandler}>Order</Button>
       </form>
     );
