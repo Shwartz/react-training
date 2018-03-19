5b3321e6f73f0f2650e2cad9410a618926031949
Shvarcs <andris.svarcs@yahoo.com>
2018-03-19 08:02:32 +0000

    Redux example simple version

--- src/Lib/date.js
+++
@@ -1,9 +0,0 @@
-export function today() {
-  const today = new Date();
-  return ((today.getDate() < 10) ? "0" : "") + today.getDate() + "/" + (((today.getMonth() + 1) < 10) ? "0" : "") + (today.getMonth() + 1) + "/" + today.getFullYear();
-}
-
-export function time() {
-  const time = new Date();
-  return ((time.getHours() < 10) ? "0" : "") + time.getHours() + ":" + ((time.getMinutes() < 10) ? "0" : "") + time.getMinutes() + ":" + ((time.getSeconds() < 10) ? "0" : "") + time.getSeconds();
-}
