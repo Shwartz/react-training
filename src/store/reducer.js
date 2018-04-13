f2c383fc452f9f0b079a170dd451009efda29da6
Shvarcs <andris.svarcs@yahoo.com>
2018-04-13 06:36:54 +0100

    Section 14 Redux: Switching off states and moving to redux

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,9 +1,33 @@
 const initialState = {
-    counter: 0
+  counter: 0
 };
 
 const reducer = (state = initialState, action) => {
-    return state;
+  switch (action.type) {
+    case 'INCREMENT':
+      return {
+        counter: state.counter + 1
+      };
+
+    case 'DECREMENT':
+      return {
+        counter: state.counter - 1
+      };
+
+    case 'ADD':
+      return {
+        counter: state.counter + action.val
+      };
+
+    case 'SUBTRACT':
+      return {
+        counter: state.counter - action.val
+      };
+
+    default:
+      return state;
+
+  }
 };
 
 export default reducer;
