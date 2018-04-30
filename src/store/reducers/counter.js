15d7b161a923a909aa8cf76ed959a6a2912ce43a
Shvarcs <andris.svarcs@yahoo.com>
2018-04-30 16:36:06 +0100

    Assignment

--- src/store/reducers/counter.js
+++ src/store/reducers/counter.js
@@ -1,8 +1,7 @@
-import * as actionTypes from './actions';
+import * as actionTypes from '../actions';
 
 const initialState = {
   counter: 0,
-  results: []
 };
 
 const reducer = (state = initialState, action) => {
@@ -28,22 +27,6 @@ const reducer = (state = initialState, action) => {
       return {
         ...state,
         counter: state.counter - action.val
-      };
-
-    // don't use push as it would change original array
-    case actionTypes.STORE_RESULT:
-      return {
-        ...state,
-        results: state.results.concat({id: new Date(), value: state.counter})
-      };
-
-    case actionTypes.DELETE_RESULT:
-      const id = 2;
-      const newArray = state.results.filter((result)=> result.id !== action.resultElId); // filter returns new array
-
-      return {
-        ...state,
-        results: newArray
       };
 
     default:
